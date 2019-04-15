4091ba38d50013f50e05970676063e1736871997
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-04-15 19:16:11 -0400

    Update ArrayInsertion.java

--- Array Insertion/ArrayInsertion.java
+++ Array Insertion/ArrayInsertion.java
@@ -15,6 +15,7 @@ public class ArrayInsertion{
 		}
 
 		afterArray[indexToInsert] = valueToInsert; 
+		
 		return afterArray;
 
 	}
@@ -26,10 +27,13 @@ public class ArrayInsertion{
 		int indexToInsert = 3; 
 		int valueToInsert = 15;
 		int[] afterArray = insertIntoArray(beforeArray, indexToInsert, valueToInsert); 
+		
 		System.out.println("Array before insertion: ");
+		
 		for(int i=0;i<beforeArray.length;i++){
 			System.out.println(beforeArray[i]); //prints out the elements in beforeArray
 		}
+		
 		System.out.println("Array after insertion of " + valueToInsert + " at position " + indexToInsert + ":");
 		for(int i=0;i<afterArray.length;i++){
 			System.out.println(afterArray[i]); //prints out the elements in afterArray
