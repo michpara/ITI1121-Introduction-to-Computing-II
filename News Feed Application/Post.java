425f50fe79ff2e267025e3ffdea01bd0d87fbff9
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-04-15 23:27:49 -0400

    fixed formatting

--- News Feed Application/Post.java
+++ News Feed Application/Post.java
@@ -12,53 +12,56 @@ public class Post implements Likeable, Comparable<Post>{
 	//constructor
 	public Post(String userName){
 
-		this.userName = userName;
-
-		timeStamp = Calendar.getInstance().getTime(); //gets the time
-
-		likes = 0; 
-
+	    this.userName = userName;
+	    timeStamp = Calendar.getInstance().getTime(); //gets the time
+	    likes = 0; 
 	}
+	
 	//gets the username
 	public String getUserName() {
+		
 	    return userName;
-    }
+        }
 	
     //gets the date and time it was added 
-    public Date getTimeStamp() {
+        public Date getTimeStamp() {
+	    
 	    return timeStamp;
-    }
+        }
 
 	//adds likes to the post
 	public void like(){
 
-		likes++; //likes increments by 1
+	    likes++; //likes increments by 1
 	}
 	
 	//returns the number of likes a post has
 	public int getLikes(){
-		return likes;
+		
+	    return likes;
 	}
 	
 	//returns if a post is popular
 	public boolean isPopular(){
 
-		if(likes>100){ 
-			return true;
-		}
-		else{
-			return false;
-		}
+	    if(likes>100){ 
+		return true;
+	    }
+		
+	    else{
+		return false;
+	    }
 	}
 	
 	//compares this Post with other
 	public int compareTo(Post other){
-  		return timeStamp.compareTo(other.timeStamp); //compares the timeStamp to the other.timeStamp
+  	    return timeStamp.compareTo(other.timeStamp); //compares the timeStamp to the other.timeStamp
   	
   	}
 	
 	//returns a string representation of the Post
 	public String toString() {
+		
     	String str = new String();
     	str = getClass().getName() + ": " + timeStamp + ", " + userName + ", likes = " + likes;
     	return  str;
