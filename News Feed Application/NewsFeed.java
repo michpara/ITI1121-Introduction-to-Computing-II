183855b659525b0176a3e84fd0283413a8af2986
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-04-15 23:34:17 -0400

    cleaned comments and code

--- News Feed Application/NewsFeed.java
+++ News Feed Application/NewsFeed.java
@@ -7,52 +7,52 @@ public class NewsFeed {
 
     //constructor
     public NewsFeed() {
-      messages = new Post[25]; 
-      size = 0; 
+        messages = new Post[25]; 
+        size = 0; 
     }
 
     //adds a Post to the NewsFeed
     public void add(Post message) {
-      if(size==MAX_SIZE){ 
-        System.out.println("Max size reached");
-        return;
-      }
+        if(size==MAX_SIZE){ 
+            System.out.println("Max size reached");
+            return;
+        }
 
-      messages[size++] = message; 
+        messages[size++] = message; 
     
     }
 
     //gets the Post at a given index
     public Post get(int index) {
-	     return messages[index];
+        return messages[index];
     }
 
     //returns the size of the NewsFeed
     public int size() {
-	     return size;
+        return size;
     }
 	  //sorts the NewsFeed
-	  public void sort(){
-			int i, j, argMin;
-			Post temp;
-			for (i = 0; i < size - 1; i++) {
-				argMin = i;
-				for (j = i + 1; j < size(); j++) {
-					if (messages[j].compareTo(messages[argMin]) < 0) {
-						argMin = j;
-					}
-				}
+    public void sort(){
+	int i, j, argMin;
+	Post temp;
+	for (i = 0; i < size - 1; i++) {
+	    argMin = i;
+		for (j = i + 1; j < size(); j++) {
+		    if (messages[j].compareTo(messages[argMin]) < 0) {
+			argMin = j;
+		    }
+		}
 
-  			temp = messages[argMin];
-  			messages[argMin] = messages[i];
-  			messages[i] = temp;
-		  }
-
-	  }
+	temp = messages[argMin];
+	messages[argMin] = messages[i];
+        messages[i] = temp;
+	}
+        
+    }
 
     //gets all the PhotoPosts on the NewsFeed
     public NewsFeed getPhotoPost(){
-      NewsFeed photoFeed = new NewsFeed();
+        NewsFeed photoFeed = new NewsFeed();
 
         for(int i=0; i < size; i++){
             if (messages[i].getClass().getName().equals("PhotoPost")){ 
@@ -68,11 +68,11 @@ public class NewsFeed {
 	      NewsFeed newFeed = new NewsFeed();
 
 	      for(int i =0;i<size();i++){
-		newFeed.add(messages[i]); 
+		  newFeed.add(messages[i]); 
 	      }
 
 	      for(int j =0;j<size();j++){
-		newFeed.add(other.messages[j]); 
+		  newFeed.add(other.messages[j]); 
 	      }
 
 
