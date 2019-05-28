c0ed5f52417a4f4fc9b0dabfbbe0839ad580277d
Michelle <michpara@yahoo.com>
2019-05-28 17:20:22 -0400

    added calendar improt

---
+++ LightsOut/LightsOut.java
@@ -0,0 +1,49 @@
+import java.util.Calendar;
+
+public class LightsOut{
+
+
+	public ArrayList<Solution> solve(int width, int height){
+
+		//instance variables
+		SolutionQueue partialSolutions = new ArrayListSolutionQueue();
+		ArrayList<Solution> solutions = new ArrayList<Solution>();
+		partialSolutions.enqueue(new Solution(width, height));
+		Solution current;
+
+		while(!partialSolutions.isEmpty()){
+			long startTime = Calendar.getInstance().getTimeInMillis();
+			current = partialSolutions.dequeue();
+			if(current.isReady() && current.isSuccessful()){
+				long endTime = Calendar.getInstance().getTimeInMillis();
+				long totalTime = endTime - startTime;
+				System.out.println("A solution was found in: " + totalTime);
+				solution.add(current);
+			}
+			else{
+				Solution currentExt = new Solution(current);
+				current.setNext(true);
+				partialSolutions.enqueue(current);
+				currentExt.setNext(false);
+				partialSolutions.enqueue(currentExt);
+			}
+		}
+		return solutions;
+
+	}
+
+	public static void main(String[] args){
+
+		int firstArg = Integer.parseInt(args[0]);
+		int secondArg = Integer.parseInt(args[1]);
+
+		ArrayList<Solution> solutionsFound = solve(firstArg, secondArg);
+		int numberOfSolutionsFound = solutionsFound.size();
+		System.out.println(numberOfSolutionsFound);
+
+		for(int i =0;i<solutionsFound.size();i++){
+			System.out.println(solutionsFound.get(i));
+		}
+
+	}
+}
