70a92f24e8367262c44a0e31b5268cb4d3cb97ca
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2020-05-12 18:11:19 -0400

    small change

--- Labs/Lab10/OrderedStructure.java
+++ Labs/Lab10/OrderedStructure.java
@@ -1,7 +1,5 @@
 public interface OrderedStructure {
-
     public abstract int size();
-
     public abstract boolean add( Comparable obj ) throws IllegalArgumentException;
     public abstract Object get( int pos ) throws IndexOutOfBoundsException;
     public abstract void remove( int pos ) throws IndexOutOfBoundsException;
